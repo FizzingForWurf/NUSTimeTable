35a8290ee541f1457fd5f1fc211a5926be87c20d
Tong Zheng Hong <zhtong315@gmail.com>
2023-06-05 17:14:05 +0800

    Add profile page

--- src/Paths.tsx
+++ src/Paths.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Timetable from './views/Timetable';
 import Login from './views/Login';
+import Profile from './views/Profile';
 
 const Paths: React.FC = () => (
   <Routes>
     <Route path="/" element={<Timetable />}></Route>
-    <Route path="/login" element={<Login invalid={false} />}></Route>
+    <Route path="/login" element={<Login />}></Route>
+    <Route path="/profile" element={<Profile />}></Route>
   </Routes>
 );
 
---
+++ src/views/Profile.tsx
@@ -0,0 +1,49 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../redux/store';
+import LoadingSpinner from './LoadingSpinner';
+import { useState } from 'react';
+import { firebaseSignOutUser } from '../Firebase/FirebaseAuth';
+import { useNavigate } from 'react-router-dom';
+import { signOutUser } from '../redux/UserSlice';
+import { toast } from 'react-hot-toast';
+
+const Profile = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const dispatch = useDispatch<AppDispatch>();
+  const curUser = useSelector((state: RootState) => state.user.user);
+
+  const handleLogout = async () => {
+    try {
+      setIsLoading(true);
+      setError('');
+      await firebaseSignOutUser();
+
+      setIsLoading(false);
+      dispatch(signOutUser());
+      toast.success('Signed out!', { position: 'top-right' });
+      navigate('/');
+    } catch (error) {
+      setIsLoading(false);
+      setError('An error occurred while signing out. Please try again later.');
+    }
+  };
+
+  return (
+    <div>
+      <p>Profile</p>
+      <p>{curUser?.displayName}</p>
+      <p>{curUser?.email}</p>
+      <p>{curUser?.uid}</p>
+      <p>{curUser?.photoURL}</p>
+
+      {error !== '' && <p>{error}</p>}
+      <button onClick={handleLogout}>Sign out</button>
+      {isLoading && <LoadingSpinner />}
+    </div>
+  );
+};
+
+export default Profile;
