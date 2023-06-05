f3a66466f7a15c063e82e7df466dec137e245eed
Tong Zheng Hong <zhtong315@gmail.com>
2023-06-05 17:06:19 +0800

    Replace react-toasitify with react-hot-toast

--- src/views/Timetable.tsx
+++ src/views/Timetable.tsx
@@ -1,16 +1,25 @@
+import { useSelector } from 'react-redux';
 import { Counter } from '../Counter';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-const notify = () => toast('Wow so easy!');
+import { toast } from 'react-hot-toast';
+import { RootState } from '../redux/store';
+import { firebaseGetCurrentUser } from '../Firebase/FirebaseAuth';
 
 export const Timetable = () => {
+  const curState = useSelector((state: RootState) => state);
+
   return (
     <>
       <h1>React TypeScript Webpack Starter Template</h1>
       <Counter />
-      <button onClick={notify}>Login</button>
-      <ToastContainer />
+      <button
+        onClick={() => {
+          toast('Wow so easy!');
+          firebaseGetCurrentUser();
+          console.log(curState);
+        }}
+      >
+        TESTING
+      </button>
     </>
   );
 };
