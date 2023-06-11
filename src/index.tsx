5187f87927f41999e15935241814105de8cb58d5
Tong Zheng Hong <zhtong315@gmail.com>
2023-06-12 00:51:33 +0800

    Follow React18 standards using ReactDom client

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
 import { Provider } from 'react-redux';
 import Store from './redux/store';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <Provider store={Store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
