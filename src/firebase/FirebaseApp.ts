557890bcd74aa8479659b05ab187b203ddd1d6e4
Tong Zheng Hong <zhtong315@gmail.com>
2023-06-15 13:34:51 +0800

    Remove json stringify when reading dotenv var

--- src/firebase/FirebaseApp.ts
+++ src/firebase/FirebaseApp.ts
@@ -1,10 +1,10 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: JSON.stringify(process.env.FIREBASE_API_KEY),
-  appId: JSON.stringify(process.env.FIREBASE_APP_ID),
-  messagingSenderId: JSON.stringify(process.env.FIREBASE_SENDER_ID),
-  measurementId: JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID), // Optional
+  apiKey: process.env.FIREBASE_API_KEY,
+  appId: process.env.FIREBASE_APP_ID,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID, // Optional
   authDomain: 'nustimetable.firebaseapp.com',
   projectId: 'nustimetable',
   storageBucket: 'nustimetable.appspot.com',
