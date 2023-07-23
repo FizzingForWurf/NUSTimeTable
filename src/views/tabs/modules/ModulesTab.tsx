22f7930dc4785b0bdf365afd05458f5db78c028d
FizzingForWurf <zhtong315@gmail.com>
2023-07-24 02:06:07 +0800

    Add module list UI connected with redux

--- src/views/tabs/modules/ModulesTab.tsx
+++ src/views/tabs/modules/ModulesTab.tsx
@@ -1,6 +1,11 @@
 import styles from './ModulesTab.scss';
 import AddModuleSvg from 'svg/add_modules.svg';
-import { Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { RootState } from 'redux/store';
+import { isEmpty } from 'lodash';
+import { getSemesterModules } from 'utils/timetableUtils';
+import ModuleItem from './ModuleItem';
 
 const EmptyModulesPage = () => {
   return (
@@ -21,18 +26,34 @@ const EmptyModulesPage = () => {
 };
 
 const ModulesTab = () => {
-  //   const modules = useSelector((state: RootState) => state.timetable.modules);
-  //   const timetableConfig = useSelector(
-  //     (state: RootState) => state.timetable.lessons
-  //   );
-  //   const currentSem = useSelector(
-  //     (state: RootState) => state.timetable.semester
-  //   );
+  const modules = useSelector((state: RootState) => state.timetable.modules);
+  const timetableConfig = useSelector(
+    (state: RootState) => state.timetable.lessons
+  );
+  const currentColors = useSelector(
+    (state: RootState) => state.timetable.colors
+  );
+  const currentSem = useSelector(
+    (state: RootState) => state.timetable.semester
+  );
 
+  const currentSemTimetable = timetableConfig[currentSem] || {};
+  if (isEmpty(currentSemTimetable)) return <EmptyModulesPage />;
+
+  const currentSemModules = getSemesterModules(currentSemTimetable, modules);
+  const currentSemColors = currentColors[currentSem] || {};
   return (
-    <div>
-      <EmptyModulesPage />
-    </div>
+    <Grid container spacing={2}>
+      {currentSemModules.map((module, index) => (
+        <Grid item xs={12} sm={6} md={4} key={index}>
+          <ModuleItem
+            module={module}
+            currentSem={currentSem}
+            colorIndex={currentSemColors[module.moduleCode] || 0}
+          />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
